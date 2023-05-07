9e63a3bcf8cbceba5d3c11dab59551c223321a74
jessica <jessica.g.adamek@gmail.com>
2023-05-07 17:09:26 -0300

    :sparkles: create and update user

---
+++ src/controllers/UserController.js
@@ -0,0 +1,71 @@
+const { hash, compare } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+const knex = require("../database/knex");
+
+class UserController {
+  async create(request, response) {
+    const { name, email, password } = request.body;
+
+    const checkUserExists = await knex("users").where({ email }).first();
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    const hashedpasswod = await hash(password, 8);
+
+    await knex("users").insert({
+      name,
+      email,
+      password: hashedpasswod,
+      isAdmin: true
+    });
+
+    return response.status(201).json();
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+
+    const user = await knex("users").where({ id }).first();
+
+    if (!user) {
+      throw new AppError("Usuário não existe");
+    }
+
+    const userWithUpdatedEmail = await knex("users").where({ email }).first();
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password) {
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha");
+    }
+
+    if (password && old_password) {
+      const checkeOldPassword = await compare(old_password, user.password);
+
+      if (!checkeOldPassword) {
+        throw new AppError("A senha antiga não confere.");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await knex("users").update({
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      updated_at: knex.fn.now()
+    }).where({ id });
+
+    return response.status(201).json();
+  }
+}
+
+module.exports = UserController;
--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ const sqliteConnection = require('./database/sqlite');
 const AppError = require("./utils/AppError");
 
 const express = require('express');
+const routes = require('./routes');
 
 sqliteConnection();
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
@@ -25,8 +27,6 @@ app.use((error, request, response, next) => {
     message: 'Internal server error'
   });
 });
-
-
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
